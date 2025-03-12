ae80b9805a283d6265cbe437fd6459693ff7aef0
JDY <wjsehdud@nate.com>
2025-03-12 20:23:48 +0900

    페이지 컴포넌트 삭제: CSR, ISR, SSG, SSR, 상세 페이지 및 레이아웃 제거 초기화작업

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,7 @@
-import { getTodos } from "@/api/todo-api";
-import Link from "next/link";
 import React from "react";
 
-const HomePage = async () => {
-  const todos = await getTodos();
-
-  return (
-    <div>
-      HomePage
-      <Link href={"/detail"}>Detail Page</Link>
-      <ul>
-        {todos.map(({ id, text, completed }) => (
-          <li key={id}>
-            {text} {completed ? "(Completed)" : "(Pending)"}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+const HomePage = () => {
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
