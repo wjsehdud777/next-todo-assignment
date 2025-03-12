eecdedb10b181ade9bc91e741be97f5adf529de6
JDY <wjsehdud@nate.com>
2025-03-12 20:36:14 +0900

    API 개선: 할 일 목록 CRUD 기능 추가 및 ID 타입을 문자열로 변경

--- api/todo-api.ts
+++ api/todo-api.ts
@@ -1,8 +1,51 @@
 import { Todo } from "@/types/todo.type";
 
+const BASE_URL = "http://localhost:3000/todos";
+
 export const getTodos = async () => {
-  const response = await fetch("http://localhost:3000/todo");
+  const response = await fetch(BASE_URL);
   const data: Todo[] = await response.json();
+
+  return data;
+};
+
+export const createTOdo = async (text: string) => {
+  const response = await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text, Completed: false }),
+  });
+
+  const data: Todo = await response.json();
+
+  return data;
+};
+
+export const deleteTodo = async (id: Todo["id"]) => {
+  const response = await fetch(`${BASE_URL}/${id}`, {
+    method: "DELETE",
+  });
+
+  const data: Todo = await response.json();
+
+  return data;
+};
+
+export const toggleTodoCompleted = async (
+  id: Todo["id"],
+  completed: Todo["completed"]
+) => {
+  const response = await fetch(`${BASE_URL}/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ completed }),
+  });
+
+  const data: Todo = await response.json();
 
   return data;
 };
